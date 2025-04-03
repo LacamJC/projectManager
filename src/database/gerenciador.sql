1bd92297609f977e461f822a77d4769148c86fa0
LacamJC <joaoramajo744@gmail.com>
2025-04-03 01:02:22 -0300

    pop

--- src/database/gerenciador.sql
+++ src/database/gerenciador.sql
@@ -1,5 +1,10 @@
 -- Criando tabela de projetos, tarefas e status
 
+CREATE TABLE IF NOT EXISTS status(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT
+);
+
 CREATE TABLE IF NOT EXISTS projeto(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     id_status INTEGER,
@@ -10,17 +15,12 @@ CREATE TABLE IF NOT EXISTS projeto(
 );
 
 CREATE TABLE IF NOT EXISTS tarefa(
-    id INTEGER AUTOINCREMENT PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     id_projeto INTEGER,
     nome TEXT,
     descricao TEXT,
     concluida INTEGER DEFAULT 0,
     FOREIGN KEY (id_projeto) REFERENCES projeto(id)
-);
-
-CREATE TABLE IF NOT EXISTS status(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT
 );
 
 -- Inserindo valores inciais para a tabela status e tornando eles imutáveis
